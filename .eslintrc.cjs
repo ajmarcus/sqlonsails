454b75d4bc986403615674c4f996c5253be5c383
Ariel Marcus <ajmarcus@gmail.com>
2024-01-11 23:09:11 -0500

    run initial format with prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,13 @@
 /* eslint-env node */
 module.exports = {
     extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended-type-checked'
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended-type-checked",
     ],
     parserOptions: {
         project: true,
         tsconfigRootDir: __dirname,
     },
-    plugins: ['@typescript-eslint'],
+    plugins: ["@typescript-eslint"],
     root: true,
 };
