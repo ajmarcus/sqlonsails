333bb01805b4b9989659dbd8cd038932496fd556
Ariel Marcus <ajmarcus@gmail.com>
2024-01-11 23:02:02 -0500

    lint using prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+/* eslint-env node */
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended-type-checked'
+    ],
+    parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+    },
+    plugins: ['@typescript-eslint'],
+    root: true,
+};
